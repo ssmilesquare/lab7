7679c123f64c117f6af086a8a7d823a225874968
ssmilesquare <ssmilesquare@gmail.com>
2021-01-05 09:56:56 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -9,3 +9,41 @@
 "ONE-NIGHT-STAND"
 "MARRIED"
 */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age,height,property;
+    string status;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age<=20){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height<160){
+            status="UNFRIEND";
+        }else if(height<175 && height>=160){
+            status="FRIEND";
+        }else{
+            cout << "Enter your property: ";
+            cin >> property;
+            if(property>69000000){
+                status="MARRIED";
+            }else{
+                status="ONE-NIGHT-STAND";
+            }
+        }
+    }else if(age<30 && age>20){
+        cout << "Enter your property: ";
+        cin >> property;
+        if(property>10000000){
+            status="BEST FRIEND";
+        }else{
+            status="UNFRIEND";
+        }
+    }else{
+        status="UNFRIEND";
+    }
+    cout << "Your status = " << status;
+}
