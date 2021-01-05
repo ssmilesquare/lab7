8fc30d3d44567db47dde391787c87a6442aea471
ssmilesquare <ssmilesquare@gmail.com>
2021-01-05 09:44:57 +0700

    lab7_2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double score){
+	if (score>90){
+		return 'A';
+	}else if(score>75 && score<=90){
+		return 'B';
+	}else if(score>60 && score<=75){
+		return 'C';
+	}else if(score>45 && score<=60){
+		return 'D';
+	}else{
+		return 'F';
+	}
+}
 
 int main(){
 	//Input the number of students
@@ -18,8 +31,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
